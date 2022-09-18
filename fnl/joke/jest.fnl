5e959c490cb853f0d1a4b697fe94ba6ae0dbf4e0
Nicholas Cellino <nacellin@buffalo.edu>
2022-09-18 15:42:24 -0400

    made test output log to current buffer as comment

--- fnl/joke/jest.fnl
+++ fnl/joke/jest.fnl
@@ -1,5 +1,6 @@
 (module joke.jest
-  {autoload {str joke.aniseed.string}})
+  {autoload {a joke.aniseed.core
+             str joke.aniseed.string}})
 
 (defn get-cmd [test]
   ["npm" "test" "--" "--testNamePattern" test])
@@ -9,13 +10,20 @@
     (each [_ value (pairs msgs)]
       (print value))))
 
+(defn log-to-buffer [_ msgs]
+  (let [bufnr (vim.api.nvim_get_current_buf)]
+    (when msgs
+      (vim.api.nvim_buf_set_lines bufnr -1 -1 false (a.map #(.. "// " $1) msgs)))))
+
 (defn execute-cmd [cmd]
   (vim.fn.jobstart
     cmd
     {:stdout_buffered true
      :stderr_buffered true
-     :on_stdout log-msgs
-     :on_stderr log-msgs}))
+     :on_stdout log-to-buffer
+     :on_stderr log-to-buffer}))
+;     :on_stdout log-msgs
+;     :on_stderr log-msgs}))
 
 (defn execute-test [test]
   (let [cmd (get-cmd test)]
--- lua/joke/jest.lua
+++ lua/joke/jest.lua
@@ -11,8 +11,9 @@ do
   _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
 end
 local autoload = (require("joke.aniseed.autoload")).autoload
-local str = autoload("joke.aniseed.string")
-do end (_2amodule_locals_2a)["str"] = str
+local a, str = autoload("joke.aniseed.core"), autoload("joke.aniseed.string")
+do end (_2amodule_locals_2a)["a"] = a
+_2amodule_locals_2a["str"] = str
 local function get_cmd(test)
   return {"npm", "test", "--", "--testNamePattern", test}
 end
@@ -28,8 +29,20 @@ local function log_msgs(_, msgs)
   end
 end
 _2amodule_2a["log-msgs"] = log_msgs
+local function log_to_buffer(_, msgs)
+  local bufnr = vim.api.nvim_get_current_buf()
+  if msgs then
+    local function _2_(_241)
+      return ("// " .. _241)
+    end
+    return vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, a.map(_2_, msgs))
+  else
+    return nil
+  end
+end
+_2amodule_2a["log-to-buffer"] = log_to_buffer
 local function execute_cmd(cmd)
-  return vim.fn.jobstart(cmd, {stdout_buffered = true, stderr_buffered = true, on_stdout = log_msgs, on_stderr = log_msgs})
+  return vim.fn.jobstart(cmd, {stdout_buffered = true, stderr_buffered = true, on_stdout = log_to_buffer, on_stderr = log_to_buffer})
 end
 _2amodule_2a["execute-cmd"] = execute_cmd
 local function execute_test(test)
